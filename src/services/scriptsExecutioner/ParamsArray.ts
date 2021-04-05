8d15f42c1060242043cbd049579fac6b2de97996
Marcos Gonalons <m.gonalons.camps@gmail.com>
2021-04-05 21:10:11 +0200

    Adjusted params

--- src/services/scriptsExecutioner/ParamsArray.ts
+++ src/services/scriptsExecutioner/ParamsArray.ts
@@ -7,10 +7,10 @@ export default function getParamsArray(): ScriptParams[] {
   const tpDistanceShortForBreakEvenSL = 1;
 
   for (const trendCandles of [180]) {
-    for (let trendDiff = 0; trendDiff < 1; trendDiff++) {
-      for (let c = 10; c < 20; c++) {
-        for (let takeProfitDistance = 12; takeProfitDistance < 30; takeProfitDistance++) {
-          for (let stopLossDistance = 4; stopLossDistance < 15; stopLossDistance++) {
+    for (let trendDiff = 0; trendDiff < 0; trendDiff++) {
+      for (let c = 12; c < 20; c++) {
+        for (let takeProfitDistance = 20; takeProfitDistance < 30; takeProfitDistance++) {
+          for (let stopLossDistance = 10; stopLossDistance < 15; stopLossDistance++) {
             arr.push({
               riskPercentage,
               stopLossDistance,
