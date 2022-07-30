017172d992b9c9f106f2bf396d6b0fd9ea9a0a47
Marcos Gonalons <m.gonalons.camps@gmail.com>
2022-07-30 21:41:49 +0200

    adjusting params

--- src/services/scriptsExecutioner/ParamsArray.ts
+++ src/services/scriptsExecutioner/ParamsArray.ts
@@ -11,10 +11,10 @@ export default function getParamsArray(): StrategyParams[] {
   const validMonths: StrategyParams["validMonths"] = [];
 
 
-  for (const maxStopLossDistance of [50,100,150,200,250].map(sl => sl * priceAdjustment)) {
-    for (const takeProfitDistance of [10,30,50,70,90,110,130,150,170,190,210,230,250].map(tp => tp * priceAdjustment)) {
-      for (const candlesAmountToBeConsideredHorizontalLevel of [5,10,15,20,25,30]) {
-        for (const priceOffset of [-30,-25,-20,-15,-10,-5,0,5,10,15]) {
+  for (const maxStopLossDistance of [300].map(sl => sl * priceAdjustment)) {
+    for (const takeProfitDistance of [10,30,50,70,90,110,130,150,170,190,210,230].map(tp => tp * priceAdjustment)) {
+      for (const candlesAmountToBeConsideredHorizontalLevel of [5,15,25,35]) {
+        for (const priceOffset of [-30,-25,-20,-15,-10,-5,0,5,10]) {
           for (const candlesAmountWithoutEMAsCrossing of [10,20,30,40]) {
             for (const tpDistanceShortForTighterSL of [0].map((tp) => tp * priceAdjustment)) {
               for (const slDistanceWhenTpIsVeryClose of [0].map((tp) => tp * priceAdjustment)) {
