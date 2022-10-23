15168931f0bd1f89462e7685f04b86a0c434ad3e
Marcos Gonalons <m.gonalons.camps@gmail.com>
2022-10-23 12:01:05 +0200

    Get params from scripts executioner for combinations tests

--- src/context/scriptsContext/scripts/EmaCrossover.ts
+++ src/context/scriptsContext/scripts/EmaCrossover.ts
@@ -12,6 +12,7 @@ export default (function f({
   persistedVars,
   isWithinTime,
   debugLog,
+  params,
   strategies
 }: StrategyFuncParameters) {
   if (balance < 0) {
@@ -22,116 +23,126 @@ export default (function f({
 
 
   function longs() {
-    const priceAdjustment = 1 / 10000;
-
-    const riskPercentage = 1;
-    const priceOffset = 75 * priceAdjustment;
-
-    const candlesAmountToBeConsideredHorizontalLevel = {
-      future: 30,
-      past: 40
+    function getParams(params: StrategyParams | null): StrategyParams {
+      if (params) {
+        return params;
+      }
+      const priceAdjustment = 1 / 10000;
+  
+      const riskPercentage = 1;
+      const priceOffset = 75 * priceAdjustment;
+  
+      const candlesAmountToBeConsideredHorizontalLevel = {
+        future: 30,
+        past: 40
+      }
+  
+      const minStopLossDistance = 50 * priceAdjustment;
+      const maxStopLossDistance = 600 * priceAdjustment;
+      const takeProfitDistance = 200 * priceAdjustment;
+      const minProfit = 999999 * priceAdjustment;
+  
+      const trailingSL = {
+        tpDistanceShortForTighterSL: 30 * priceAdjustment,
+        slDistanceWhenTpIsVeryClose: -90 * priceAdjustment
+      }
+  
+      const trailingTP = {
+        slDistanceShortForTighterTP: 100 * priceAdjustment,
+        tpDistanceWhenSlIsVeryClose: -20 * priceAdjustment
+      }
+  
+      const candlesAmountWithoutEMAsCrossing = 12;
+      const maxSecondsOpenTrade = 0 * 24 * 60 * 60;
+  
+      const validHours: StrategyParams["validHours"] = [];
+      const validMonths: StrategyParams["validMonths"] = [];
+      const validDays: StrategyParams["validDays"] = [];
+  
+      return {
+        validHours,
+        validDays,
+        validMonths,
+        riskPercentage,
+        minStopLossDistance,
+        maxStopLossDistance,
+        candlesAmountToBeConsideredHorizontalLevel,
+        priceOffset,
+        takeProfitDistance,
+        minProfit,
+        trailingSL,
+        trailingTP,
+        candlesAmountWithoutEMAsCrossing,
+        maxSecondsOpenTrade,
+      };
     }
-
-    const minStopLossDistance = 50 * priceAdjustment;
-    const maxStopLossDistance = 600 * priceAdjustment;
-    const takeProfitDistance = 200 * priceAdjustment;
-    const minProfit = 99999999 * priceAdjustment;
-
-    const trailingSL = {
-      tpDistanceShortForTighterSL: 30 * priceAdjustment,
-      slDistanceWhenTpIsVeryClose: 90 * priceAdjustment
-    }
-
-    const trailingTP = {
-      slDistanceShortForTighterTP: 100 * priceAdjustment,
-      tpDistanceWhenSlIsVeryClose: -20 * priceAdjustment
-    }
-
-    const candlesAmountWithoutEMAsCrossing = 12;
-    const maxSecondsOpenTrade = 0 * 24 * 60 * 60;
-
-    const validHours: StrategyParams["validHours"] = [];
-    const validMonths: StrategyParams["validMonths"] = [];
-    const validDays: StrategyParams["validDays"] = [];
-
-    const params = {
-      validHours,
-      validDays,
-      validMonths,
-      riskPercentage,
-      minStopLossDistance,
-      maxStopLossDistance,
-      candlesAmountToBeConsideredHorizontalLevel,
-      priceOffset,
-      takeProfitDistance,
-      minProfit,
-      trailingSL,
-      trailingTP,
-      candlesAmountWithoutEMAsCrossing,
-      maxSecondsOpenTrade,
-    };
 
     strategies.find(s => s.name === "EMA Crossover Longs")!.func({
       candles, orders, trades, balance, currentDataIndex, spread,
       createOrder, closeOrder, persistedVars, isWithinTime, debugLog,
-      params, strategies
+      params: getParams(params || null), strategies
     });
   }
 
   function shorts() {
-    const priceAdjustment = 1 / 10000;
-
-    const riskPercentage = 1;
-    const priceOffset = 150 * priceAdjustment;
-
-    const candlesAmountToBeConsideredHorizontalLevel = {
-      future: 50,
-      past: 15
+    function getParams(params: StrategyParams | null): StrategyParams {
+      if (params) {
+        return params;
+      }
+      const priceAdjustment = 1 / 10000;
+  
+      const riskPercentage = 1;
+      const priceOffset = 150 * priceAdjustment;
+  
+      const candlesAmountToBeConsideredHorizontalLevel = {
+        future: 50,
+        past: 15
+      }
+  
+      const minStopLossDistance = 20 * priceAdjustment;
+      const maxStopLossDistance = 1000 * priceAdjustment;
+      const takeProfitDistance = 350 * priceAdjustment;
+      const minProfit = 120 * priceAdjustment;
+  
+      const trailingSL = {
+        tpDistanceShortForTighterSL: 0 * priceAdjustment,
+        slDistanceWhenTpIsVeryClose: 0 * priceAdjustment
+      }
+  
+      const trailingTP = {
+        slDistanceShortForTighterTP: 40 * priceAdjustment,
+        tpDistanceWhenSlIsVeryClose: -100 * priceAdjustment
+      }
+  
+      const candlesAmountWithoutEMAsCrossing = 0;
+      const maxSecondsOpenTrade = 0 * 24 * 60 * 60;
+  
+      const validHours: StrategyParams["validHours"] = [];
+      const validMonths: StrategyParams["validMonths"] = [];
+      const validDays: StrategyParams["validDays"] = [];
+  
+      return {
+        validHours,
+        validDays,
+        validMonths,
+        riskPercentage,
+        minStopLossDistance,
+        maxStopLossDistance,
+        candlesAmountToBeConsideredHorizontalLevel,
+        priceOffset,
+        takeProfitDistance,
+        minProfit,
+        trailingSL,
+        trailingTP,
+        candlesAmountWithoutEMAsCrossing,
+        maxSecondsOpenTrade,
+      };
     }
-
-    const minStopLossDistance = 20 * priceAdjustment;
-    const maxStopLossDistance = 1000 * priceAdjustment;
-    const takeProfitDistance = 350 * priceAdjustment;
-    const minProfit = 120 * priceAdjustment;
-
-    const trailingSL = {
-      tpDistanceShortForTighterSL: 0 * priceAdjustment,
-      slDistanceWhenTpIsVeryClose: 0 * priceAdjustment
-    }
-
-    const trailingTP = {
-      slDistanceShortForTighterTP: 40 * priceAdjustment,
-      tpDistanceWhenSlIsVeryClose: -100 * priceAdjustment
-    }
-
-    const candlesAmountWithoutEMAsCrossing = 0;
-    const maxSecondsOpenTrade = 0 * 24 * 60 * 60;
-
-    const validHours: StrategyParams["validHours"] = [];
-    const validMonths: StrategyParams["validMonths"] = [];
-    const validDays: StrategyParams["validDays"] = [];
-
-    const params = {
-      validHours,
-      validDays,
-      validMonths,
-      riskPercentage,
-      minStopLossDistance,
-      maxStopLossDistance,
-      candlesAmountToBeConsideredHorizontalLevel,
-      priceOffset,
-      takeProfitDistance,
-      minProfit,
-      trailingSL,
-      trailingTP,
-      candlesAmountWithoutEMAsCrossing,
-      maxSecondsOpenTrade,
-    };
 
     strategies.find(s => s.name === "EMA Crossover Shorts")!.func({
       candles, orders, trades, balance, currentDataIndex, spread,
       createOrder, closeOrder, persistedVars, isWithinTime, debugLog,
-      params, strategies
+      params: getParams(params || null), strategies
     });
   }
 
@@ -155,6 +166,7 @@ function f({
   persistedVars,
   isWithinTime,
   debugLog,
+  params,
   strategies
 }) {
 `.trim(),
